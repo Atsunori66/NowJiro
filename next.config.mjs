a1328c33bc645177f2e0c1f4c2050116ebbb498f
Atsunori66 <atsunori.smith@gmail.com>
2025-10-28 01:04:10 +0900

    fixed theme toggle

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    // ビルド時のESLintを無効化（開発効率を優先）
-    ignoreDuringBuilds: true,
-  },
+  // eslint: {
+  //   // ビルド時のESLintを無効化（開発効率を優先）
+  //   ignoreDuringBuilds: true,
+  // },
   typescript: {
     // ビルド時のTypeScriptエラーを無視
     ignoreBuildErrors: true,
