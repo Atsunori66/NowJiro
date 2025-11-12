c8a69c1203c1c69bab5826e7ff6f4adec7526019
Atsunori66 <atsunori.smith@gmail.com>
2025-11-12 23:37:50 +0900

    fixed manifest 2

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -2,13 +2,13 @@ import type { MetadataRoute } from 'next'
  
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: '今すぐ行ける二郎',
+    name: '今行ける二郎',
     short_name: 'NowJiro',
     description: '今すぐ行ける二郎系ラーメン店を確認できるアプリ',
     start_url: '/',
     display: 'standalone',
-    background_color: '#ffffff',
-    theme_color: '#000000',
+    background_color: '#00a6ff',
+    theme_color: '#ffd700',
     icons: [
       {
         src: '/icon-500x500.png',
